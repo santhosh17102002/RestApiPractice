5576da88c6c9e38ff10ad2978408bcb1ab6e55e4
SANTHOSH TADEPALLI <109502221+santhosh17102002@users.noreply.github.com>
2024-03-01 15:42:59 +0530

    Add files via upload

---
+++ users.js
@@ -0,0 +1,39 @@
+const users = [
+{
+
+id: 1,
+name: 'T santhosh',
+
+email: 'Tsatya@gmail.com',
+
+type: 'admin'
+
+},
+
+{
+
+id: 2,
+
+name: 'Srinivas',
+
+email: 'vasu@gmail.com',
+
+status: 'guest'
+
+},
+
+{
+
+id: 3,
+
+name: 'Thrishank',
+
+email: 'thrishank@gmail.com',
+
+status: 'admin'
+
+}
+
+];
+
+module.exports = users;
